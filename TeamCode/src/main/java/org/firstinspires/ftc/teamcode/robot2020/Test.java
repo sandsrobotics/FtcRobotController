11fa3ef77240bab49d5b2f82a32032acf645b8dc
Om Patel <drompatel5@gmail.com>
2020-10-06 20:10:46 -0400

    fxing open cv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot2020/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot2020/Test.java
@@ -17,25 +17,14 @@ public class Test extends LinearOpMode
         robot = new Robot(hardwareMap,telemetry,gamepad1,gamepad2,true, true, true);
 
         robot.vision.initAll(false, true);
-        if(robot.debug_dashboard)robot.vision.startDashboardCameraStream(24);
 
         waitForStart();
-
-        robot.vision.activate();
 
         while (opModeIsActive())
         {
             robot.startTelemetry();
-            robot.vision.findAnyTrackable(true);
-            robot.vision.printTelemetry();
-            robot.movement.moveForTeleOp(gamepad1);
-
-            if(gamepad1.a) robot.movement.findBlueTowerGoal(45,22.5);
-
+            robot.addTelemetryDouble("number of rings", robot.vision.getNumberOfRings());
             robot.sendTelemetry();
         }
-
-        robot.vision.deactivate();
-        if(robot.debug_dashboard)robot.vision.stopDashboardCameraStream();
     }
 }
