a62923b42c9d974d1d872658006871ea1260cace
sands <senseandsoundrobotics@gmail.com>
2021-01-24 16:12:33 -0500

    Disabled unused op-modes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/EasyOpenCVExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/EasyOpenCVExample.java
@@ -21,6 +21,7 @@
 
 package org.firstinspires.ftc.teamcode.tests;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -39,6 +40,7 @@ import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
 
 @TeleOp
+@Disabled
 public class EasyOpenCVExample extends LinearOpMode
 {
     OpenCvWebcam webcam;
