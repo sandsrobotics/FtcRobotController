b5d8a072d2bde994748efb8838a9bf0b8e601eca
sands <senseandsoundrobotics@gmail.com>
2021-01-31 20:28:43 -0500

    Working DF robotics I2c driver

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/holo/UltrasonicRangeTest.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.holo;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/*
+ * OpMode for testing DFRobotics Ultrasonic Distance sensor driver
+ */
+@TeleOp(name = "Ultrasonic Range Test", group = "Tests")
+public class UltrasonicRangeTest extends LinearOpMode
+{
+    public void runOpMode()
+    {
+        DFR304Range distSensor = hardwareMap.get(DFR304Range.class, "distSensor");
+
+        DFR304Range.Parameters parameters = new DFR304Range.Parameters();
+        parameters.maxRange = DFR304Range.MaxRange.CM500;
+        parameters.measureMode = DFR304Range.MeasureMode.ACTIVE;
+        distSensor.initialize(parameters);
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            //distSensor.measureRange();
+            telemetry.addData("Distance CM", distSensor.getDistanceCm());
+            telemetry.addData("Distance IN", distSensor.getDistanceIn());
+            telemetry.addData("Temperature F", distSensor.getTemperatureF());
+            telemetry.addData("Temperature C", distSensor.getTemperatureC());
+            telemetry.addData("Config", (distSensor.readOneByte(DFR304Range.Register.CFG_INDEX) & 0xFF));
+            telemetry.addData("Manufacturer", distSensor.getManufacturer());
+            telemetry.update();
+            idle();
+        }
+
+    }
+}
