a42fb520c0f1ef1a8dd268bc775a76d97970649b
omzz15 <drompatel5@gmail.com>
2021-03-11 21:44:04 -0500

    made tune PIDF and changed Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot2020/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot2020/Launcher.java
@@ -369,7 +369,7 @@ class LauncherSettings
 
     //power shots
     double powerShotRPM = 3500;
-    double[] powerShotXPos = {30, 35, 40}; //this is how far the robot has to be from goal to launch - IN INCHES!!!
+    double[] powerShotXPos = {25, 30, 35}; //this is how far the robot has to be from goal to launch - IN INCHES!!!
     double[] powerShotPosTol = {.5,.5,.5}; // the tolerance of position and angle required
 
     LauncherSettings(){}
